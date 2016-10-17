987a2f901d32055a2ca81e1e3968ac329621c4ad
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 21:16:35 -0700

    added state, added addTodo and removeTodo handlers to update state; bound addTodo in TodoForm and removeTodo in TodoList to the handlers in app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,47 @@ const ReactDOM = require('react-dom');
 const Title = require('./Title.js');
 const TodoForm = require('./TodoForm.js');
 const TodoList = require('./TodoList');
+window.id = 0;
 
-const App = () => (
-  <div>
-    <Title />
-    <TodoForm />
-    <TodoList />
-  </div>
-);
+
+class App extends React.Component{
+	constructor(props){
+		// pass props to parent class
+		super(props);
+		// set initial state
+		this.state = {
+			data: []
+		}
+	}
+	// add todo handler
+	addTodo(value){
+		const todo = {text: value, id: window.id++}
+		// update data
+		this.state.data.push(todo);
+		// update state
+		this.setState({data: this.state.data});
+	}
+	// handle remove
+	removeTodo(id){
+		// Filter all todos except the one to be removed
+		const remaining = this.state.data.filter((todo) => {
+			if(todo.id !== id) return todo;
+		});
+		// update state with filter
+		this.setState({data: remaining});
+	}
+	render(){	
+		return (
+		  <div>
+		    <Title />
+		    <TodoForm addTodo={this.addTodo.bind(this)} />
+		    <TodoList 
+		    	todos={this.state.data}
+		    	remove={this.removeTodo.bind(this)}
+		    />
+		  </div>
+		)
+	}
+};
 
 ReactDOM.render(<App />, document.getElementById('app'))
