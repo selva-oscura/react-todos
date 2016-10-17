fd3c859470601583200208e9b433f72146354cc8
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 20:59:11 -0700

    added checkbox and remove event handler to Todo Component

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,22 @@
 const React = require('react');
 
-const Todo = (props) => (
+const Todo = ({todo, remove}) => (
   <li className='collection-item row'> 
-    <span className="col s9 push-s1">{props.text}</span>
+  	<span className="col s1">
+  		<i className="material-icons left">check_box_outline_blank</i>
+  	</span>
+    <span className="col s10 push">{todo.text}</span>
+    <span className="col s1">
+    	<button
+    		className="btn-floating btn waves-effect waves-light right red"
+    		onClick={() => {
+    			console.log('remove todo.id', todo.id);
+    			remove(todo.id);
+				}
+			}>
+    		<i className="material-icons right">clear</i>
+    	</button>
+    </span>
   </li>
 );
 
