e0784058eed705b9e735a81e19a66d33df15673a
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 21:17:33 -0700

    removed fake data

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,19 +2,9 @@ const React = require('react');
 const Todo = require('./Todo.js');
 
 const TodoList = ({todos, remove}) => {
-  // todos === fake data for starting off.....
-  var todos = [
-    'learn React',
-    'look into this.state',
-    'learn Flux',
-    'integrate app with Firebase',
-    'overthrow small first-world country'
-  ].map((todo, index)=> { return {text:todo, id: index}})
-  console.log('todos', todos);
   const todoList = todos.map((todo) => {
 		return (<Todo todo={todo} key={todo.id} remove={remove} />);
 	});
-
   return (
     <div className="row">
       <div className="col s12">
