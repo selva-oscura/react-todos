5ce14a2c563ab5c7fb1011ec1d0bf552f004a7e0
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 19:38:39 -0700

    added event handler to button to get value of input; added reference for pulling the value of input; layout changes to form;

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,15 +1,32 @@
 const React = require('react');
 
-const TodoForm = () => (
-  <div className="row">
-    <div className="col s9">
-      <input id="new_todo" type="text" placeholder="Things to do....." />
-    </div>
-    <button className="btn waves-effect waves-light col s3" id="submit_todo" type="submit">
-      Add
-      <i className="material-icons right">send</i>
-    </button>
-  </div>
-)
+const TodoForm = ({addTodo}) => {
+	let input;
+	return (
+		<div className="row">
+			<div className="col s11">
+				<input 
+					id="text" 
+					type="text" 
+					placeholder="Things to do....." 
+					ref={node => {input = node;}} 
+				/>
+			</div>
+			<div className="col s1">
+				<button 
+					id="submit_todo" 
+					type="submit" 
+					className="btn-floating btn waves-effect waves-light right teal" 
+					onClick={() => {
+						console.log('input.value', input.value)
+						addTodo(input.value);
+						input.value = "";
+					}}>
+					<i className="material-icons right">add</i>
+				</button>
+			</div>
+		</div>
+	);
+}
 
 module.exports = TodoForm;
