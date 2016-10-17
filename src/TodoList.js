cf147b8436ed43be7a0d18823381998a047701bf
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 21:32:43 -0700

    added passing of checked

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,9 @@
 const React = require('react');
 const Todo = require('./Todo.js');
 
-const TodoList = ({todos, remove}) => {
+const TodoList = ({todos, check, remove}) => {
   const todoList = todos.map((todo) => {
-		return (<Todo todo={todo} key={todo.id} remove={remove} />);
+		return (<Todo todo={todo} key={todo.id} remove={remove} check={check} />);
 	});
   return (
     <div className="row">
