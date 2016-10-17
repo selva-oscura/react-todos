f4589665bb87b5f2fdb58be4e960dcb955d012ca
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 22:49:26 -0700

    display all unchecked todos before checked todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,13 +3,21 @@ const Todo = require('./Todo.js');
 
 const TodoList = ({todos, check, remove}) => {
   const todoList = todos.map((todo) => {
-		return (<Todo todo={todo} key={todo.id} remove={remove} check={check} />);
+  	if(!todo.checked){
+			return (<Todo todo={todo} key={todo.id} remove={remove} check={check} />);
+  	}
+	});
+	const doneList = todos.map((todo) => {
+		if(todo.checked){
+			return (<Todo todo={todo} key={todo.id} remove={remove} check={check} />);
+		}
 	});
   return (
     <div className="row">
       <div className="col s12">
         <ul className="collection">
           {todoList}
+          {doneList}
         </ul>
       </div>
     </div>
