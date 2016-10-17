5348b4b0fa247392149c5ad80900d58efdfdce2f
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 19:41:29 -0700

    renamed var names (props => todos; todos array => todoList; fakeData => todos (to overwrite passed todos until we have live data))

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,24 @@
 const React = require('react');
 const Todo = require('./Todo.js');
 
-const TodoList = (props) => {
-  var todos = [];
-  var fakeData = [
+const TodoList = (todos) => {
+  var todoList = [];
+  // todos === fake data for starting off.....
+  var todos = [
     'learn React',
     'look into this.state',
     'learn Flux',
     'integrate app with Firebase',
     'overthrow small first-world country'
   ];
-  for(let i = 0; i<fakeData.length; i++){
-    todos.push(<Todo key={i} text={fakeData[i]} />);
+  for(let i = 0; i<todos.length; i++){
+    todoList.push(<Todo key={i} text={todos[i]} />);
   }
   return (
     <div className="row">
       <div className="col s12">
         <ul className="collection">
-          {todos}
+          {todoList}
         </ul>
       </div>
     </div>
