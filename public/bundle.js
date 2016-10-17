03dfa6e13c625c42bbaf461c4aaa5b3542fc49d9
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-16 21:18:02 -0700

    updated bundle

--- public/bundle.js
+++ public/bundle.js
@@ -46,21 +46,81 @@
 
 	'use strict';
 
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(34);
 	var Title = __webpack_require__(172);
 	var TodoForm = __webpack_require__(173);
 	var TodoList = __webpack_require__(174);
+	window.id = 0;
 
-	var App = function App() {
-	  return React.createElement(
-	    'div',
-	    null,
-	    React.createElement(Title, null),
-	    React.createElement(TodoForm, null),
-	    React.createElement(TodoList, null)
-	  );
-	};
+	var App = function (_React$Component) {
+		_inherits(App, _React$Component);
+
+		function App(props) {
+			_classCallCheck(this, App);
+
+			// set initial state
+			var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+			// pass props to parent class
+
+
+			_this.state = {
+				data: []
+			};
+			return _this;
+		}
+		// add todo handler
+
+
+		_createClass(App, [{
+			key: 'addTodo',
+			value: function addTodo(value) {
+				var todo = { text: value, id: window.id++ };
+				// update data
+				this.state.data.push(todo);
+				// update state
+				this.setState({ data: this.state.data });
+			}
+			// handle remove
+
+		}, {
+			key: 'removeTodo',
+			value: function removeTodo(id) {
+				// Filter all todos except the one to be removed
+				var remaining = this.state.data.filter(function (todo) {
+					if (todo.id !== id) return todo;
+				});
+				// update state with filter
+				this.setState({ data: remaining });
+			}
+		}, {
+			key: 'render',
+			value: function render() {
+				return React.createElement(
+					'div',
+					null,
+					React.createElement(Title, null),
+					React.createElement(TodoForm, { addTodo: this.addTodo.bind(this) }),
+					React.createElement(TodoList, {
+						todos: this.state.data,
+						remove: this.removeTodo.bind(this)
+					})
+				);
+			}
+		}]);
+
+		return App;
+	}(React.Component);
+
+	;
 
 	ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
 
@@ -21555,10 +21615,14 @@
 	  var remove = _ref.remove;
 
 	  // todos === fake data for starting off.....
-	  var todos = ['learn React', 'look into this.state', 'learn Flux', 'integrate app with Firebase', 'overthrow small first-world country'].map(function (todo, index) {
-	    return { text: todo, id: index };
-	  });
-	  console.log('todos', todos);
+	  // var todos = [
+	  //   'learn React',
+	  //   'look into this.state',
+	  //   'learn Flux',
+	  //   'integrate app with Firebase',
+	  //   'overthrow small first-world country'
+	  // ].map((todo, index)=> { return {text:todo, id: index}})
+	  // console.log('todos', todos);
 	  var todoList = todos.map(function (todo) {
 	    return React.createElement(Todo, { todo: todo, key: todo.id, remove: remove });
 	  });
