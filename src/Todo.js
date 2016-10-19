52cc1d82e570dde9c9a66489f968e97092cf7248
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-19 02:06:35 -0700

    added listid data to remove and check calls

--- src/Todo.js
+++ src/Todo.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const Todo = ({todo, check, remove}) => (
   <li className='collection-item row'> 
-  	<span className="col s1"
-  		onClick={() => {
-  			check(todo.id);
-  		}
-  	}>
-  		<i className="material-icons left">
-  			{todo.checked ? "done" : "check_box_outline_blank"}
-  		</i>
-  	</span>
-    <span className="col s10 push">{todo.text}</span>
-    <span className="col s1">
-    	<button
-    		className="btn-floating btn waves-effect waves-light right right red darken-4"
-    		onClick={() => {
-    			remove(todo.id);
-				}
-			}>
-    		<i className="material-icons">clear</i>
-    	</button>
+    <span className="col s2"
+      onClick={() => {
+        check(todo.listId, todo.todoId);
+      }
+    }>
+      <i className="material-icons left">
+        {todo.checked ? "done" : "check_box_outline_blank"}
+      </i>
+    </span>
+    <span className="col s8">{todo.text}</span>
+    <span className="col s2">
+      <button
+        className="btn-floating btn waves-effect waves-light right right red darken-4"
+        onClick={() => {
+          remove(todo.listId, todo.todoId);
+        }
+      }>
+        <i className="material-icons">clear</i>
+      </button>
     </span>
   </li>
 );
