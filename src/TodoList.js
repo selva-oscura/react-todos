89b83392f26d1d665f4c4c4caf427c3633f28e92
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-19 02:02:48 -0700

    added todoList.listId and updated todo.id to todo.todoId; moved TodoForm to bottom of TodoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import TodoForm from './TodoForm';
 import Todo from './Todo';
 
-const TodoList = ({todos, check, remove}) => {
-  const todoList = todos.map((todo) => {
-  	if(!todo.checked){
-			return (<Todo todo={todo} key={todo.id} remove={remove} check={check} />);
-  	}
-	});
-	const doneList = todos.map((todo) => {
-		if(todo.checked){
-			return (<Todo todo={todo} key={todo.id} remove={remove} check={check} />);
-		}
-	});
+const TodoList = ({todoList, check, remove, addTodo}) => {
+  const todos = todoList.items;
+  const pendingList = todos.map((todo) => {
+    if(!todo.checked){
+      return (<Todo todo={todo} key={todo.todoId} listId={todoList.listId} remove={remove} check={check} />);
+    }
+  });
+  const doneList = todos.map((todo) => {
+    if(todo.checked){
+      return (<Todo todo={todo} key={todo.todoId} listId={todoList.listId} remove={remove} check={check} />);
+    }
+  });
   return (
-    <div className="row">
-      <div className="col s12">
-        <ul className="collection">
-          {todoList}
-          {doneList}
-        </ul>
-      </div>
+    <div className="col s12 m6 l4">
+      <h3>{todoList.name}</h3>
+      <ul className="collection">
+        {pendingList}
+        {doneList}
+        <TodoForm listId={todoList.listId} addTodo={addTodo} />
+      </ul>
     </div>
   )
 }
