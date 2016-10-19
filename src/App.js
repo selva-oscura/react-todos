2e7edb995b14f84ec77e3faf7fe995bb19e52a84
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-19 02:09:44 -0700

    updated window.id to window.listId and window.todoId; added addTodoList handler; added listId to lists and todos and methods to addTodo, removeTodo, and toggleChecked parameters and adjusted update of state to take into account new data shape

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Title from './Title';
-import TodoForm from './TodoForm';
+import TodoListForm from './TodoListForm';
+import TodoLists from './TodoLists';
 import TodoList from './TodoList';
-window.id = 0;
+window.listId = 0;
+window.todoId = 0;
 
 
 class App extends React.Component{
@@ -15,41 +17,80 @@ class App extends React.Component{
 			data: []
 		}
 	}
-	// add todo handler
-	addTodo(value){
-		const todo = {text: value, id: window.id++, checked:false}
-		// update data
-		this.state.data.push(todo);
+
+	// add list handler
+	addTodoList(value){
+		// get data
+		const list = {name: value, listId: window.listId++, items:[]}
+		// update data with new list
+		this.state.data.push(list);
 		// update state
 		this.setState({data: this.state.data});
 	}
-	// handle remove
-	removeTodo(id){
-		// Filter all todos except the one to be removed
-		const remaining = this.state.data.filter((todo) => {
-			if(todo.id !== id) return todo;
+
+	// add todo handler
+	addTodo(listId, value){
+		// get data
+		const todo = {text: value, listId: listId, todoId: window.todoId++, checked:false}
+		this.state.data.forEach(function(list, i){
+			// select proper list
+			if(list.listId===listId){
+				// add new todo to that list
+				list.items.push(todo);
+			}
 		});
-		// update state with filter
-		this.setState({data: remaining});
-	}
-	toggleChecked(id){
 		// update data
-		var todos = this.state.data;
-		todos.map((todo) => {
-			if(todo.id===id) todo.checked = !todo.checked;
-			return todo;
-		});
-		this.setState({data: todos});
+		this.setState({data: this.state.data});
+	}
+
+	// handle remove
+	removeTodo(listId, todoId){
+		// get data
+		const data = this.state.data;
+		data.forEach(function(list, i){
+			// select proper list
+			if(list.listId===listId){
+				var updatedList = {
+					name: list.name,
+					listId: list.listId
+				};
+				// Filter all todos except the one to be removed for selected list
+				updatedList.items = list.items.filter((todo) => {
+					if(todo.todoId !== todoId) return todo;
+				});
+				// replace old selected list with updated list
+				data[i] = updatedList;
+			}
+		})
+		// update state with filter
+		this.setState({data: data});
+	}
+	toggleChecked(listId, todoId){
+		// get data
+		var data = this.state.data;
+		data.forEach(function(list){
+			// select proper list
+			if(list.listId===listId){
+				list.items.map((todo) => {
+					// update selected todo in selected list
+					if(todo.todoId===todoId) todo.checked = !todo.checked;
+					return todo;
+				})
+			}
+		})
+		// update data
+		this.setState({data: data});
 	}
 	render(){	
 		return (
 		  <div>
 		    <Title />
-		    <TodoForm addTodo={this.addTodo.bind(this)} />
-		    <TodoList 
-		    	todos={this.state.data}
+		    <TodoListForm addTodoList={this.addTodoList.bind(this)} />
+		    <TodoLists 
+		    	todoLists={this.state.data}
 		    	remove={this.removeTodo.bind(this)}
 		    	check={this.toggleChecked.bind(this)}
+		    	addTodo={this.addTodo.bind(this)}
 		    />
 		  </div>
 		)
