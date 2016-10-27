326b060cfd0ac883f3246ab79f43719685b6b97e
Carol St.Louis <stlouis_c@yahoo.com>
2016-10-26 20:00:00 -0700

    realigned 2nd line of title

--- src/Title.js
+++ src/Title.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Title = () => (
   <div className="row">
-    <h2 className="col s12 title">So Many Things... So Little Time...</h2>
+    <h2 className="col s12 title">
+    	So Many Things... <br />
+    	<span className="right">So Little Time...</span>
+    </h2>
   </div>
 )
 
